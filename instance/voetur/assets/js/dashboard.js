0c517e8fb02407efbcf1599a3ed6f4c970d61a34
Bruno Hiroshi Murakami <brunohiroshi@gmail.com>
2025-05-22 10:28:46 -0300

    ajuestes de suprimir lexi

--- instance/voetur/assets/js/dashboard.js
+++ instance/voetur/assets/js/dashboard.js
@@ -21,18 +21,18 @@ export async function renderDashboard() {
                 </div>
                 <span style="font-size: 0.75rem; margin-top: 0.25rem;">Lançar</span>
               </a>
-              <a role="button" href="#chat" class="br-button br-button--secondary" style="flex: 0 0 auto; display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 0.25rem;">
+              <!-- <a role="button" href="#chat" class="br-button br-button--secondary" style="flex: 0 0 auto; display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 0.25rem;">
                 <div style="width: 80px; height: 80px; background-color: #f2f2f2; border: 1px solid #ccc; border-radius: 12px; display: flex; align-items: center; justify-content: center;">
                   <i class="bi bi-chat-dots" style="font-size: 2.5rem;"></i>
                 </div>
                 <span style="font-size: 0.75rem; margin-top: 0.25rem;">LexiDecis</span>
-              </a>
-              <a role="button" href="#vtc-indicador-pmr" class="br-button br-button--tertiary" style="flex: 0 0 auto; display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 0.25rem;">
+              </a> -->
+              <!-- <a role="button" href="#vtc-indicador-pmr" class="br-button br-button--tertiary" style="flex: 0 0 auto; display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 0.25rem;">
                 <div style="width: 80px; height: 80px; background-color: #f2f2f2; border: 1px solid #ccc; border-radius: 12px; display: flex; align-items: center; justify-content: center;">
                   <i class="bi bi-bar-chart" style="font-size: 2.5rem;"></i>
                 </div>
                 <span style="font-size: 0.75rem; margin-top: 0.25rem;">KPIs</span>
-              </a>
+              </a> -->
               <a role="button" href="#vtc-financeiro-gestor" class="br-button br-button--tertiary" style="flex: 0 0 auto; display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 0.25rem;">
                 <div style="width: 80px; height: 80px; background-color: #f2f2f2; border: 1px solid #ccc; border-radius: 12px; display: flex; align-items: center; justify-content: center;">
                   <i class="bi bi-wallet2" style="font-size: 2.5rem;"></i>
